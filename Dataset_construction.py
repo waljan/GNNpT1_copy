1dbf4dfc3f1f8a9e1a0ee728c4cc627922fe8c86
waljan <MeinGithubPasswort96>
2019-11-04 12:38:01 +0100

    graph labels adjusted

--- Dataset_construction.py
+++ Dataset_construction.py
@@ -42,9 +42,9 @@ class DataConstructor():
 
         # add graph label
         if "abnormal" in filename:
-            graph_class = 1
+            graph_class = [1,0]
         elif "normal" in filename:
-            graph_class = 0
+            graph_class = [0,1]
         else:
             print("the filename has not the correct format")
         y = torch.tensor([graph_class], dtype=torch.float)
@@ -57,14 +57,27 @@ class DataConstructor():
 
 
     def split(self, k):
+        """
+        This function splits the pt1 graph data set into train validation and test
+        according to the file dataset_split.csv. This file contains the information about
+        how to split the data in a 4-fold cross validation.
 
+        :param k: integer between 0 and 3 (which fold of the 4-fold cross validation)
+        :return: dictionary that has the img (e.g. img6) as key and "train", "val" or "test" as value
+        """
+
+        # open the csv file
         with open("dataset_split.csv") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=",")
             line_count = 0
             dic = {}
+
+            # iterate over every row of the csv file
             for row in csv_reader:
+                # ignore the header
                 if line_count == 0:
                     line_count += 1
+                # use a dictionary to save the information about how to split the data into train test and val
                 else:
                     dic[row[0]] = [row[k+1]]
         return dic
@@ -72,13 +85,24 @@ class DataConstructor():
 
     # get a list of all filenames
     def raw_file_names(self):
+        """
+        this function creates a list of all the gxl filenames in the directory "graphs/paper-graphs/distance-based_10_13_14_35"
+        :return: list of gxl-filenames
+        """
         r_file_names = []
+        # iterate over every file in the folder
         for file in os.listdir("graphs/paper-graphs/distance-based_10_13_14_35"):
-            if file.endswith(".gxl"):
-                r_file_names.append(file)
+            if file.endswith(".gxl"): #check if its a gxl file
+                r_file_names.append(file) #add the filename to the list
+        #return the list of gxl filenames
         return r_file_names
 
     def get_data_list(self, k=0):
+        """
+        this function creates data objects
+        :param k:
+        :return:
+        """
         # create a list of data objcts
         data_split = self.split(k)
         train_data_list = []
@@ -152,3 +176,4 @@ if __name__ == "__main__":
     train_data = DataLoader(train_data_list, batch_size=32)
     for batch in train_data:
         print(batch.num_graphs)
+        print(batch)
