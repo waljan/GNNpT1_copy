e925036c8f8b8022be4a8c3ea472062d8eb9620e
waljan <MeinGithubPasswort96>
2019-11-06 17:51:00 +0100

    GraphSAGE and GraphSAGEWithJK added

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear
-from torch_geometric.nn import GCNConv, global_mean_pool, JumpingKnowledge
+from torch_geometric.nn import GCNConv, SAGEConv, global_mean_pool, JumpingKnowledge
 
 
 class GCN(torch.nn.Module):
@@ -50,6 +50,9 @@ class GCN(torch.nn.Module):
         return self.__class__.__name__
 
 class GCNWithJK(torch.nn.Module):
+    # Graph Convolutional Neural Network similar to the one introduced by Kipf and Welling.
+    # https://github.com/rusty1s/pytorch_geometric/blob/master/benchmark/kernel/gcn.py
+    # jumping Knowledge (Xu et al.
     def __init__(self, num_layers, num_input_features, hidden, mode='cat'):
         super(GCNWithJK, self).__init__()
         self.conv1 = GCNConv(num_input_features, hidden)
@@ -88,4 +91,75 @@ class GCNWithJK(torch.nn.Module):
     def __repr__(self):
         return self.__class__.__name__
 
+
+class GraphSAGE(torch.nn.Module):
+    def __init__(self, num_input_features, num_layers, hidden):
+        super(GraphSAGE, self).__init__()
+        self.conv1 = SAGEConv(num_input_features, hidden)
+        self.convs = torch.nn.ModuleList()
+        for i in range(num_layers - 1):
+            self.convs.append(SAGEConv(hidden, hidden))
+        self.lin1 = Linear(hidden, hidden)
+        self.lin2 = Linear(hidden, 2)
+
+    def reset_parameters(self):
+        self.conv1.reset_parameters()
+        for conv in self.convs:
+            conv.reset_parameters()
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = F.relu(self.conv1(x, edge_index))
+        for conv in self.convs:
+            x = F.relu(conv(x, edge_index))
+        x = global_mean_pool(x, batch)
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1)
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+
+class GraphSAGEWithJK(torch.nn.Module):
+    def __init__(self, num_input_features, num_layers, hidden, mode='cat'):
+        super(GraphSAGEWithJK, self).__init__()
+        self.conv1 = SAGEConv(num_input_features, hidden)
+        self.convs = torch.nn.ModuleList()
+        for i in range(num_layers - 1):
+            self.convs.append(SAGEConv(hidden, hidden))
+        self.jump = JumpingKnowledge(mode)
+        if mode == 'cat':
+            self.lin1 = Linear(num_layers * hidden, hidden)
+        else:
+            self.lin1 = Linear(hidden, hidden)
+        self.lin2 = Linear(hidden, 2)
+
+    def reset_parameters(self):
+        self.conv1.reset_parameters()
+        for conv in self.convs:
+            conv.reset_parameters()
+        self.jump.reset_parameters()
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = F.relu(self.conv1(x, edge_index))
+        xs = [x]
+        for conv in self.convs:
+            x = F.relu(conv(x, edge_index))
+            xs += [x]
+        x = self.jump(xs)
+        x = global_mean_pool(x, batch)
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1)
+
+    def __repr__(self):
+        return self.__class__.__name__
 
