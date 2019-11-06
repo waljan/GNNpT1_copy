3c71332e73a9f52f33f7291f24a854bdcc7c4ce9
waljan <MeinGithubPasswort96>
2019-11-06 11:44:58 +0100

    GCNWithJK added

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear
-from torch_geometric.nn import GCNConv, global_mean_pool
+from torch_geometric.nn import GCNConv, global_mean_pool, JumpingKnowledge
 
 
 class GCN(torch.nn.Module):
@@ -42,11 +42,50 @@ class GCN(torch.nn.Module):
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.lin2(x)
         # output = F.log_softmax(x, dim=-1)
-        output = F.softmax(x, dim=-1) #get output between 0 and 1
+        output = F.log_softmax(x, dim=-1) #get output between 0 and 1
         return output
 
     def __repr__(self):
         #for getting a printable representation of an object
+        return self.__class__.__name__
+
+class GCNWithJK(torch.nn.Module):
+    def __init__(self, num_layers, num_input_features, hidden, mode='cat'):
+        super(GCNWithJK, self).__init__()
+        self.conv1 = GCNConv(num_input_features, hidden)
+        self.convs = torch.nn.ModuleList()
+        for i in range(num_layers - 1):
+            self.convs.append(GCNConv(hidden, hidden))
+        self.jump = JumpingKnowledge(mode)
+        if mode == 'cat':
+            self.lin1 = Linear(num_layers * hidden, hidden)
+        else:
+            self.lin1 = Linear(hidden, hidden)
+        self.lin2 = Linear(hidden, 2)
+
+    def reset_parameters(self):
+        self.conv1.reset_parameters()
+        for conv in self.convs:
+            conv.reset_parameters()
+        self.jump.reset_parameters()
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = F.relu(self.conv1(x, edge_index))
+        xs = [x]
+        for conv in self.convs:
+            x = F.relu(conv(x, edge_index))
+            xs += [x]
+        x = self.jump(xs)
+        x = global_mean_pool(x, batch)
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        return F.log_softmax(x, dim=-1)
+
+    def __repr__(self):
         return self.__class__.__name__
 
 
