c76f67df5dd566390c3d29fc7c0849d2f9f0cc98
waljan <jannis.wallau@bluewin.ch>
2019-11-08 09:46:24 +0100

    base-dataset added

--- plotGraph.py
+++ plotGraph.py
@@ -5,7 +5,7 @@ from PIL import Image
 import read_gxl as readgxl
 
 
-def plot_Graph(filename):
+def plot_Graph(folder, filename):
     """
     this function takes a gxl file and a image and plots the cell graph on top of the image
     :param filename: gxl file that stores the graph
@@ -19,9 +19,13 @@ def plot_Graph(filename):
 
     img = np.asarray(img)
     # get the attributes of every node
-    attr_dic =  readgxl.get_node_features(filename)
+    attr_dic =  readgxl.get_node_features(folder, filename)
+
     # get the coordinates of every node
-    coordinates = [[attr_dic[node][4], attr_dic[node][5]] for node in attr_dic]
+    if folder == "graphs/paper-graphs/distance-based_10_13_14_35/":
+        coordinates = [[attr_dic[node][4], attr_dic[node][5]] for node in attr_dic]
+    elif folder == "graphs/base-dataset/":
+        coordinates = [[attr_dic[node][33], attr_dic[node][34]] for node in attr_dic]
     # scale the node coordinates to the image size
     for i in range(len(coordinates)):
         coordinates[i][0]*=(img.shape[1]-1)
@@ -35,8 +39,8 @@ def plot_Graph(filename):
 
 
     nx.draw(G, coordinates, with_labels=False, node_shape=".", node_color="orangered", node_size=100, width=2, edge_color="orange" )
-    # plt.show()
-    plt.savefig("../../images/" + filename.replace(".gxl","-Graph.png"), format="PNG")
+
+    # plt.savefig("../../images/" + filename.replace(".gxl","-Graph.png"), format="PNG")
     plt.show()
 
     # plt.imshow(img)
@@ -54,20 +58,23 @@ def plot_Graph(filename):
 
 
 if __name__ == "__main__":
+    folder = "graphs/paper-graphs/distance-based_10_13_14_35/"
+    folder = "graphs/base-dataset/"
+
     filename = "img0_0_normal.gxl"
-    plot_Graph(filename)
+    plot_Graph(folder, filename)
     filename = "img0_1_normal.gxl"
-    plot_Graph(filename)
+    plot_Graph(folder, filename)
     filename = "img0_2_normal.gxl"
-    plot_Graph(filename)
+    plot_Graph(folder, filename)
     filename = "img0_3_normal.gxl"
-    plot_Graph(filename)
+    plot_Graph(folder, filename)
     filename = "img0_12_abnormal.gxl"
-    plot_Graph(filename)
+    plot_Graph(folder, filename)
     filename = "img0_13_abnormal.gxl"
-    plot_Graph(filename)
+    plot_Graph(folder, filename)
     filename = "img0_14_abnormal.gxl"
-    plot_Graph(filename)
+    plot_Graph(folder, filename)
     filename = "img0_15_abnormal.gxl"
-    plot_Graph(filename)
+    plot_Graph(folder, filename)
 
