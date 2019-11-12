e2c737c29b3bffe9700c3d360c4fc16ca3ea0e6c
waljan <jannis.wallau@bluewin.ch>
2019-11-12 11:34:45 +0100

    feature and num_nodes histograms

--- Dataset_construction.py
+++ Dataset_construction.py
@@ -190,7 +190,7 @@ class DataConstructor():
 
 if __name__ == "__main__":
     folder = "graphs/paper-graphs/distance-based_10_13_14_35/"
-    # folder = "graphs/base-dataset/"
+    folder = "graphs/base-dataset/"
     filename = "img0_0_normal.gxl"
 
     raw_data= DataConstructor()
@@ -205,3 +205,58 @@ if __name__ == "__main__":
     for batch in train_data:
         print(batch.num_graphs)
         print(batch)
+
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    for f in range(len(train_data_list[0].x[0])):
+        f_vec = []
+
+        for graph in train_data_list:
+            for nd in range(len(graph.x)):
+                f_vec.append(graph.x[nd][f])
+
+        plt.subplot(2, len(train_data_list[0].x[0])//2+1, f+1)
+        plt.hist(f_vec)
+        plt.title("f"+ str(f))
+
+    plt.show()
+
+
+    for f in range(len(train_data_list[0].x[0])):
+        f_vec_normal = []
+        f_vec_abnormal = []
+
+        for graph in train_data_list:
+
+            if graph.y[0][0].item() == 1:  # abnormal
+                for nd in range(len(graph.x)):
+                    f_vec_abnormal.append(graph.x[nd][f])
+
+            elif graph.y[0][0].item() == 0: #normal
+                for nd in range(len(graph.x)):
+                    f_vec_normal.append(graph.x[nd][f])
+        # print(len(f_vec_abnormal), len(f_vec_normal))
+        plt.subplot(2, len(train_data_list[0].x[0])//2+1, f+1)
+        plt.hist(f_vec_normal, alpha=0.5, label="n", density=True)
+        plt.hist(f_vec_abnormal, alpha=0.5, label="a", density=True)
+        plt.legend()
+        plt.title("f"+ str(f))
+
+    plt.show()
+
+
+    num_nd_ab = []
+    num_nd_n = []
+    for graph in train_data_list:
+        if graph.y[0][0].item() == 1:
+            num_nd_ab.append(graph.num_nodes)
+        if graph.y[0][0].item() == 0:
+            num_nd_n.append(graph.num_nodes)
+    bins = np.linspace(0,600, 20)
+    plt.hist(num_nd_n, bins, alpha=0.5, label="normal")
+    plt.hist(num_nd_ab, bins, alpha=0.5, label="abnormal")
+    plt.title("number of nodes per class")
+    plt.legend()
+    plt.show()
