7cda472a402d72ab346e5c04fb100e8b441f4a0a
waljan <jannis.wallau@bluewin.ch>
2019-11-14 15:34:31 +0100

    NMP added, not yet working

--- model.py
+++ model.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python
+
 import torch
 import torch.nn.functional as F
 from torch.nn import Linear, init
-from torch_geometric.nn import GCNConv, SAGEConv, GraphConv, JumpingKnowledge
+from torch_geometric.nn import GCNConv, SAGEConv, GraphConv, NNConv, JumpingKnowledge
 from torch_geometric.nn import global_mean_pool, TopKPooling, global_max_pool
 
+# own modules
+from MLP import MLP
 
 from GraphConvolutions import OwnGConv
 
@@ -285,4 +289,52 @@ class GraphNN(torch.nn.Module):
         return F.log_softmax(x, dim=-1)
 
     def __repr__(self):
+        return self.__class__.__name__
+
+
+class NMP(torch.nn.Module):
+    """
+    Graph Convolutional Neural Network similar to the one introduced by Kipf and Welling.
+    https://github.com/rusty1s/pytorch_geometric/blob/master/benchmark/kernel/gcn.py
+    """
+    def __init__(self, num_layers, num_input_features, hidden, nn):
+        super(NMP, self).__init__()
+        self.conv1 = NNConv(num_input_features, hidden, nn, aggr="add")
+        self.convs = torch.nn.ModuleList()
+        for i in range(num_layers - 1):
+            self.convs.append(NNConv(hidden, hidden, nn, aggr="add"))
+        self.lin1 = Linear(hidden, hidden)  #linear layer
+        self.lin2 = Linear(hidden, 2)       #linear layer, output layer, 2 classes
+        self.reset_parameters()
+
+    def reset_parameters(self):     #reset all conv and linear layers except the first GCNConv layer
+        print("reset parameters")
+        self.conv1.reset_parameters()
+        for conv in self.convs:
+            conv.reset_parameters()  # .reset_parameters() is method of the torch_geometric.nn.GCNConv class
+        self.lin1.reset_parameters()    # .reset_parameters() is method of the torch.nn.Linear class
+        self.lin2.reset_parameters()
+
+    def forward(self, data):
+        x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
+
+        #graph convolutions and relu activation
+        x = F.relu(self.conv1(x, edge_index, edge_attr))
+        for conv in self.convs:
+            x = F.relu(conv(x, edge_index, edge_attr))
+
+        # global mean pooling: the feature vector of every node of one graph are summed and the mean is taken
+        # if there are 30 graphs in the batch and the feature vector has length 6, the resulting x has shape [30, 6]
+        x = global_mean_pool(x, batch)
+
+        #linear layers, activation function, dropout and softmax
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin2(x)
+        # output = F.log_softmax(x, dim=-1)
+        output = F.log_softmax(x, dim=-1) #get output between 0 and 1
+        return output
+
+    def __repr__(self):
+        #for getting a printable representation of an object
         return self.__class__.__name__
