820c457b89d659cfdaff1702865152765017924d
waljan <jannis.wallau@bluewin.ch>
2019-11-15 13:47:06 +0100

    new way of data augmentation

--- Data_Augmentation.py
+++ Data_Augmentation.py
@@ -3,9 +3,9 @@
 import copy
 import numpy as np
 
-def augment(train_data_list, n=4):
+def augment_every_nd(train_data_list, n=4):
     """
-    augments the data set by randomly adding small values (between 0 and 1) to every node
+    augments the data set by randomly adding small values (between 0 and 1) to **every** node
 
     data object representing a graph:
     Data(edge_attr=[n_Nodes, 2*n_edge_attr], edge_index=[2, 2*n_Nodes], pos=[N, 2], x=[n_Nodes, n_node_features], y=[1, num_classes])
@@ -41,3 +41,37 @@ def augment(train_data_list, n=4):
             train_data_list_aug.append(train_data_list_cp[graph])
 
     return train_data_list_aug
+
+
+def augment(train_data_list, n=2):
+    """
+    augments the data set by randomly adding small values (drawn from normal distribution with mean 0 and sd=1) to a random number of features of randomly selected nodes
+
+    data object representing a graph:
+    Data(edge_attr=[n_Nodes, 2*n_edge_attr], edge_index=[2, 2*n_Nodes], pos=[N, 2], x=[n_Nodes, n_node_features], y=[1, num_classes])
+    e.g.: Data(edge_attr=[224, 1], edge_index=[2, 224], pos=[112, 2], x=[112, 33], y=[1, 2])
+
+    :param train_data_list: list of graph data objects
+    :param n: factor by which the dataset will be increased
+    :return: train_data_list_aug: list of graph data objects containing the original and the changed graphs
+    """
+
+    train_data_list_aug = copy.deepcopy(train_data_list)
+    train_data_list_cp = copy.deepcopy(train_data_list)
+
+    for i in range(n-1):
+        for graph in range(len(train_data_list)):  # iterate over every graph
+            num_nodes = len(train_data_list[graph].x)
+            r_nd = np.random.randint(0, num_nodes, (np.random.randint(1, num_nodes, 1))) # get the indices of a random number of nodes
+
+            for nd in r_nd: # iterate over every node
+                num_f = len(train_data_list[graph].x[nd])
+                r_f = np.random.randint(0, num_f, (np.random.randint(1, num_f, 1))) # get the indices of a random number of features
+                for f in r_f:
+                    train_data_list_cp[graph].x[nd][f] += np.random.normal(0,1)
+
+
+            train_data_list_aug.append(train_data_list_cp[graph])
+
+    return train_data_list_aug
+
