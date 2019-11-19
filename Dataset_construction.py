3250f53cfdb9688338f3d9b86f90827a7725f8fe
waljan <jannis.wallau@bluewin.ch>
2019-11-19 11:08:21 +0100

    comments updated

--- Dataset_construction.py
+++ Dataset_construction.py
@@ -17,12 +17,16 @@ class DataConstructor():
         """
         this function takes a gxl-file as input and creates a graph as it is used in pytorch Geometric
         :param filename: gxl file that stores a graph
+        :param folder:
         :return python object modeling a single graph with various attributes
         """
-        # initialize tree
+        # initialize tree from chosen folder/file
         tree = ET.ElementTree(file=folder + filename)
         root = tree.getroot()
 
+        # get edges
+        ##############################
+        # initialize tree from paper-graph dataset to get edge information (because base dataset doesnt has edges)
         tree2 = ET.ElementTree(file= "pT1_dataset/graphs/paper-graphs/distance-based_10_13_14_35/" + filename)
         root2 = tree2.getroot()
 
@@ -34,51 +38,53 @@ class DataConstructor():
         # create a tensor needed to construct the graph
         edge_index = torch.tensor(edge_list, dtype=torch.long)
 
+        # get node features and position
+        ##############################
         # initialize the list
         all_node_features = []
         all_node_positions = []
-        # iterate over every node
-        for node in root.iter("node"):
-            # get the feature vector of every node
-            feature_vec = [float(value.text) for feature in node for value in feature]
+
+        for node in root.iter("node"):        # iterate over every node
+            feature_vec = [float(value.text) for feature in node for value in feature]      # get the feature vector of every node
 
             if folder == "pT1_dataset/graphs/paper-graphs/distance-based_10_13_14_35/":
-                node_position = feature_vec[4:]
-                feature_vec = feature_vec[:4]   # the last two entries corespond to the node-coordinates in the image
-                                                # the coordinates are not used as features
+                node_position = feature_vec[4:]         # the last two entries corespond to the node-coordinates in the image
+                feature_vec = feature_vec[:4]           # the coordinates are not used as features
+
 
             if folder == "pT1_dataset/graphs/base-dataset/":
-                node_position = feature_vec[33:]
-                feature_vec = feature_vec[:33]
+                node_position = feature_vec[33:]        # the last two entries corespond to the node-coordinates in the image
+                feature_vec = feature_vec[:33]          # the coordinates are not used as features
 
-            # and append it to the list
+            # append the feature vec and position of the current node to the list
             all_node_features.append(feature_vec)
             all_node_positions.append(node_position)
 
-        # create a tensor needed to construct the graph
+        x = torch.tensor(all_node_features, dtype=torch.float)      # create a tensor needed to construct the graph
+        pos = torch.tensor(all_node_positions, dtype=torch.float)   # create a tensor needed to construct the graph
 
-        x = torch.tensor(all_node_features, dtype=torch.float)
-
-        # add graph label
+        # get the label of the class
+        ###########################
         if "abnormal" in filename:
             graph_class = [1,0]
         elif "normal" in filename:
             graph_class = [0,1]
         else:
             print("the filename has not the correct format")
-        y = torch.tensor([graph_class], dtype=torch.float)
+        y = torch.tensor([graph_class], dtype=torch.float)          # create a tensor needed to construct the graph
 
-        pos = torch.tensor(all_node_positions, dtype=torch.float)
 
-        # compute the length of every edge based on the cooridnates of the nodes
+        # get the edge feature (length of the edge based on the node coordinates)
+        ############################
         distances = []
-        for edge in edge_index:
-            positions = pos[edge]
-            distances.append([torch.dist(positions[0], positions[1], p=2)])#compute L2 norm
+        for edge in edge_index:             # iterate over every edge
+            positions = pos[edge]           # get the coordinates of the nodes connected by the edge
+            distances.append([torch.dist(positions[0], positions[1], p=2)])         # compute L2 norm
 
-        edge_attr = torch.tensor(distances, dtype=torch.float)
+        edge_attr = torch.tensor(distances, dtype=torch.float)      # create a tensor needed to construct the graph
 
         # construct the graph
+        #############################
         graph = Data(x=x, y=y, edge_attr =edge_attr, edge_index=edge_index.t().contiguous(), pos=pos)
 
 
@@ -86,11 +92,11 @@ class DataConstructor():
 
     def split(self, k):
         """
-        This function splits the pt1 graph data set into train validation and test
+        This function splits the pt1 graph data set into train, validation and test
         according to the file dataset_split.csv. This file contains the information about
         how to split the data in a 4-fold cross validation.
 
-        :param k: integer between 0 and 3 (which fold of the 4-fold cross validation)
+        :param k: integer between 0 and 3 (which of the 4 possible splits in the 4-fold cross validation)
         :return: dictionary that has the img (e.g. img6) as key and "train", "val" or "test" as value
         """
 
@@ -102,36 +108,37 @@ class DataConstructor():
 
             # iterate over every row of the csv file
             for row in csv_reader:
-                # ignore the header
-                if line_count == 0:
+                if line_count == 0:         # ignore the header
                     line_count += 1
-                # use a dictionary to save the information about how to split the data into train test and val
-                else:
+                else:                       # use a dictionary to save the information about how to split the data into train test and val
                     dic[row[0]] = [row[k+1]]
         return dic
 
-    # get a list of all filenames
+
     def raw_file_names(self, folder):
         """
-        this function creates a list of all the gxl filenames in the directory "graphs/paper-graphs/distance-based_10_13_14_35"
+        this function creates a list of all the gxl filenames located in the directory "folder"
+        :param folder: defines the folder in which the gxl files are located
         :return: list of gxl-filenames
         """
+
         r_file_names = []
-        # iterate over every file in the folder
-        for file in os.listdir(folder):
-            if file.endswith(".gxl"): #check if its a gxl file
-                r_file_names.append(file) #add the filename to the list
-        #return the list of gxl filenames
-        return r_file_names
+
+        for file in os.listdir(folder):         # iterate over every file in the folder
+            if file.endswith(".gxl"):           # check if its a gxl file
+                r_file_names.append(file)       # add the filename to the list
+
+        return r_file_names                     # return the list of gxl filenames
 
     def get_data_list(self, folder, k=0):
         """
-        this function creates lists of data objects separated into train val and test
+        this function creates lists of data objects (data lists) separated into train val and test
         :param k: k can take values from 0 to 3 and defines which datasplit in the 4-fold cross validation is used
+        :param folder: determines which dataset is used (paper or base dataset)
         :return: lists of data objects for train val and test
         """
 
-        data_split = self.split(k) #get dictionary that tells how to split the data into train val and test
+        data_split = self.split(k) # get dictionary that tells how to split the data into train val and test
 
         # initialize list
         train_data_list = []
@@ -140,16 +147,17 @@ class DataConstructor():
 
         pattern = "_"
         a=0
-        # iterate over every filename
-        for file in self.raw_file_names(folder):
+
+        for file in self.raw_file_names(folder):        # iterate over every filename
+
             # create the data object representing the graph stored in the file
             try:
                 data = self.get_graph(folder, file)
             except:
                 print(file, "could not be loaded")
                 continue
-            img = re.split(pattern, file)[0] # get the image from which this graph was sampled
-            split = data_split[img][0]  # determine whether this graph belongs to train val or test
+            img = re.split(pattern, file)[0]            # get the image from which this graph was sampled
+            split = data_split[img][0]                  # determine whether this graph belongs to train val or test
 
             # add the data object for the graph to the corresponding list
             if split == "test":
@@ -162,41 +170,7 @@ class DataConstructor():
         return train_data_list, val_data_list, test_data_list
 
 
-# class GraphDataset(InMemoryDataset):
-#     def __init__(self, root, transform=None, pre_transform=None):
-#         super(GraphDataset, self).__init__(root, transform, pre_transform)
-#         self.data, self.slices = torch.load(self.processed_paths[0])
-#
-#     @property
-#     def raw_file_names(self):
-#         r_file_names = []
-#         try:
-#             for filename in os.listdir("./graph/paper-graphs/distance-based_10_13_14_35/"):
-#                 if filename.endswith(".gxl"):
-#                     r_file_names.append(filename)
-#         except:
-#             print("you have to save the raw gxl files in the following folder:")
-#             print("graph/paper-graphs/distance-based_10_13_14_35/")
-#
-#         return r_file_names
-#
-#     @property
-#     def processed_file_names(self):
-#         return ['data.pt']
-#
-#     def download(self):
-#         pass
-#         # Download to `self.raw_dir`.
-#
-#     def process(self):
-#         # Read data into huge `Data` list.
-#         data_list = []
-#         for filename in self.raw_file_names():
-#             data = get_graph(filename)
-#             data_list.append(data)
-#
-#         data, slices = self.collate(data_list)
-#         torch.save((data, slices), self.processed_paths[0])
+
 
 
 if __name__ == "__main__":
@@ -204,8 +178,8 @@ if __name__ == "__main__":
     folder = "pT1_dataset/graphs/base-dataset/"
     filename = "img0_0_normal.gxl"
 
-    raw_data= DataConstructor()
-    train_data_list, val_data_list, test_data_list = raw_data.get_data_list(folder)
+    raw_data= DataConstructor() # initialize instance
+    train_data_list, val_data_list, test_data_list = raw_data.get_data_list(folder) # get data lists for train, val and test
     print(len(train_data_list))
     print(train_data_list[0])
     print("feature vec:",train_data_list[0].x[0])
@@ -222,11 +196,9 @@ if __name__ == "__main__":
     import numpy as np
     from statistics import stdev, mean
 
-    # fig, axs = plt.subplots(4, len(train_data_list[0].x[0])//4 + 1)
-    # r = 0
-    # c = 0
 
-    plt.rc("font", size=5)
+    # plot feature distributions (normal and abnormal glands together)
+    plt.rc("font", size=5)  # change font size
 
     for f in range(len(train_data_list[0].x[0])):
         f_vec = []
@@ -235,14 +207,6 @@ if __name__ == "__main__":
             for nd in range(len(graph.x)):
                 f_vec.append(graph.x[nd][f].item())
 
-        # axs[r, c].hist(f_vec)
-        # r += 1
-        # c += 1
-        # if r == 4:
-        #     r=0
-        # if c == len(train_data_list[0].x[0])//4 + 1:
-        #     c=0
-
         plt.subplot(4, len(train_data_list[0].x[0])//4+1, f+1)
         plt.hist(f_vec, density=True)
         plt.title("f"+ str(f) + "  std: " + str(stdev(f_vec))[:4] + "  mean: " + str(mean(f_vec))[:4])
@@ -250,7 +214,7 @@ if __name__ == "__main__":
     # plt.tight_layout()
     plt.show()
 
-
+    # plot feature distributions (normal vs abnormal glands)
     for f in range(len(train_data_list[0].x[0])):
         f_vec_normal = []
         f_vec_abnormal = []
@@ -273,7 +237,7 @@ if __name__ == "__main__":
     plt.tight_layout()
     plt.show()
 
-
+    # plot number of nodes (normal vs abnormal glands)
     num_nd_ab = []
     num_nd_n = []
     for graph in train_data_list:
--- Save_Data_Objects.py
+++ Save_Data_Objects.py
@@ -8,24 +8,25 @@ from Data_Augmentation import augment as aug
 
 def save_obj(folder, augment):
     """
-    saves training, validation and test data lists for all 4 cross validation runs to a file
+    saves training, validation, test and augmented training data lists for all 4 cross validation runs to a file
     :param folder: determeines the folder which contains the raw gxl files
-    :param augment: intiger that determines by which factor the dataset should be augmented, augment==0 means no augmentation
+    :param augment: integer that determines by which factor the dataset should be augmented, augment==0 means no augmentation
 
-    the list all is saved to the file data_lists.pickle
-    all contains 4 internal lists (for train, val, test, train_augmented) of length 4 (4 fold cross validation)
-    The first graph in the val set in run 2 of the cross validation is accessed by all[1][2][0]
+    the list "all_lists" is saved to the file data_lists.pickle
+    all_lists contains 4 internal lists (for train, val, test, train_augmented) of length 4 (4 fold cross validation)
+    The first graph in the val set in run 2 of the cross validation is accessed by all_lists[1][2][0]
 
-    all[set][CV-run][graph]
+    all_lists[set][CV-run][graph]
     """
     print("load data")
-    raw_data = DataConstructor()
+    raw_data = DataConstructor()            # create instance of the class DataConstructor()
 
-    all_lists=[[],[],[],[]]
+    all_lists=[[],[],[],[]]                 # initialize the list that will contain the four data lists
     print("create data lists")
-    for k in range(4):
+    for k in range(4):                      # 4 iterations for the 4 fold cross validation
         print("k:", k)
         train_data_list, val_data_list, test_data_list = raw_data.get_data_list(folder, k=k)  # split the data into train val and test
+
         # augment data by adding/subtracting small random values from node features
         if augment > 1:
             train_data_list_aug = aug(train_data_list, n=augment)
@@ -34,6 +35,7 @@ def save_obj(folder, augment):
         all_lists[1].append(val_data_list)
         all_lists[2].append(test_data_list)
 
+    # save the data lists
     if folder == "pT1_dataset/graphs/paper-graphs/distance-based_10_13_14_35/":
         print("save datalists to Paper_Data_Lists.pickle")
         pickle_out = open("Paper_Data_Lists.pickle", "wb")
@@ -48,6 +50,16 @@ def save_obj(folder, augment):
         print("done")
 
 def load_obj(folder):
+    """
+    Function that loads the data lists from the pickle file
+    :param folder: determines whether the data lists from the paper-dataset or from the base dataset are loaded
+    :return: returns the data lists
+
+    all_lists contains 4 internal lists (train, val, test, train_augmented) of length 4 (4 fold cross validation)
+    The first graph in the val set in run 2 of the cross validation is accessed by all_lists[1][2][0]
+
+    all_lists[set][CV-run][graph]
+    """
     if folder == "pT1_dataset/graphs/paper-graphs/distance-based_10_13_14_35/":
         pickle_in = open("Paper_Data_Lists.pickle", "rb")
         all_lists = pickle.load(pickle_in)
@@ -61,12 +73,20 @@ def load_obj(folder):
         return all_lists
 
 if __name__ == "__main__":
+
+    # choose folder
+    ################################
     # folder = "pT1_dataset/graphs/paper-graphs/distance-based_10_13_14_35/"
     folder = "pT1_dataset/graphs/base-dataset/"
-    #
+
+    # save data lists to pickle file
+    ################################
     # augment=20
     # save_obj(folder, augment)
 
+
+    # load data list from pickle file
+    ################################
     all_lists = load_obj(folder)
     print(type(all_lists))
     print(all_lists[0][2][3])
