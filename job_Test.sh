1799b35187b501e02bb6161176ac82d19c4a3538
waljan <jannis.wallau@bluewin.ch>
2020-01-06 10:32:33 +0100

    modified #SBATCH

--- job_Test.sh
+++ job_Test.sh
@@ -2,31 +2,36 @@
 
 #SBATCH --mail-user=jannis.wallau@students.unibe.ch
 #SBATCH --mail-type=fail,end
-#SBATCH --output=./out/optim/slurm-r10-it100-%j.out
-#SBATCH --output=./out/optim/slurm-r10-it100-%j.out
+#SBATCH --output=./out/optim/slurm-test-r10-it100-%j.out
+#SBATCH --output=./out/optim/slurm-test-r10-it100-%j.out
 
-#SBATCH --job-name="HP-Opt-100iter"
+#SBATCH --job-name="test-100runs"
 
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:gtx1080ti:1
+#SBATCH --constraint=gtx1080
 #SBATCH --mem=4000M
-#SBATCH --time=20:00:00
+#SBATCH --time=04:00:00
 
 #SBATCH --no-requeue
 
 ###########################################
 source ~/anaconda3/etc/profile.d/conda.sh
 #conda activate GNNpT1Env
-conda activate GNNEnv
+echo "activate env"
+conda activate GNNpT1Env
+#conda activate GNNEnv #contains updated pytorch and torchvision
 #module load GCC/7.3.0-2.30
 #module load GCC/8.3.0
+#echo "load modules"
 module load GCC/5.4.0-2.26
-module load gcccuda
+#module load gcccuda
 module load CUDA/10.1.243
 
 #python -c "import torch; print(torch.__version__)"
 #python -c "import torch; print(torch.cuda.is_available())"
 #echo $PATH
+echo "export path"
 export PATH=/usr/local/cuda/bin:$PATH
 #echo $PATH
 #echo $LD_LIBRARY_PATH
