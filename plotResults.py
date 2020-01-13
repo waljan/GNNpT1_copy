3226e6bb2aa04f29ed50ae6252c6efdd36c8c042
waljan <jannis.wallau@bluewin.ch>
2020-01-13 16:55:06 +0100

    train val plot improved and new plot added

--- plotResults.py
+++ plotResults.py
@@ -21,6 +21,7 @@ def plot_train_val(m, folder):
             dataset = "base/"
         filename = m + "_test_data.csv"
         path = "out/" + dataset + m + "/" + filename
+        # read test_data to get mean and sd of every data split
         with open(path, "r") as test_res:
             data = list(csv.reader(test_res))
             print("sd and mean across all test splits")
@@ -29,57 +30,40 @@ def plot_train_val(m, folder):
             print("sd and mean of fold " + str(fold))
             print("sd:", data[fold+1][0])
             print("mean:", data[fold+1][1])
-
-        plt.rc("font", size=5)
-
-        # training accuracy
-        filename = m + "_train_acc_fold" + str(fold) + ".csv"
-        path = "out/" + dataset + m + "/" + filename
-        data = np.genfromtxt(path, delimiter=",")
-        plt.subplot(2,2,1)
-        x = range(len(data[0,:]))
-        for i in range(len(data[:,0])):
-            y = data[i,:]
-            plt.plot(x, y, color=str(100), alpha=0.2)
-            plt.title(dataset + m + "/fold" + str(fold) + "/train_acc")
-
-        # training loss
-        filename = m + "_train_loss_fold" + str(fold) + ".csv"
-        path = "out/" + dataset + m + "/" + filename
-        data = np.genfromtxt(path, delimiter=",")
-
-        plt.subplot(2,2,3)
-        x = range(len(data[0,:]))
-        for i in range(len(data[:,0])):
-            y = data[i,:]
-            plt.plot(x, y, color=str(100), alpha=0.2)
-            plt.title(dataset + m + "/fold" + str(fold) + "/train_loss")
+            # test_accs = data[fold+1][2:]
 
 
-        # validationa accuracy
-        filename = m + "_val_acc_fold" + str(fold) + ".csv"
-        path = "out/" + dataset + m + "/" + filename
-        data = np.genfromtxt(path, delimiter=",")
 
-        plt.subplot(2,2,2)
-        x = range(len(data[0,:]))
-        for i in range(len(data[:,0])):
-            y = data[i,:]
-            plt.plot(x, y, color=str(100), alpha=0.2)
-            plt.title(dataset + m + "/fold" + str(fold) + "/val_acc")
+        # filename = m + "_train_loss_fold" + str(fold) + ".csv"
+        # path = "out/" + dataset + m + "/" + filename
+        # trainloss = np.genfromtxt(path, delimiter=",")
 
 
-        # validationa loss
-        filename = m + "_val_loss_fold" + str(fold) + ".csv"
-        path = "out/" + dataset + m + "/" + filename
-        data = np.genfromtxt(path, delimiter=",")
 
-        plt.subplot(2,2,4)
-        x = range(len(data[0,:]))
-        for i in range(len(data[:,0])):
-            y = data[i,:]
-            plt.plot(x, y, color=str(100), alpha=0.2)
-            plt.title(dataset + m + "/fold" + str(fold) + "/val_loss")
+
+        plt.rc("font", size=7)
+        subpl=0
+
+        # plot train_acc, train_loss, val_acc and val_loss
+        for k in ["_train_acc_fold", "_val_acc_fold", "_train_loss_fold", "_val_loss_fold"]:
+
+            subpl +=1
+            filename = m + k + str(fold) + ".csv"
+            path = "out/" + dataset + m + "/" + filename
+            data = np.genfromtxt(path, delimiter=",")
+            plt.subplot(2,2,subpl)
+            x = range(len(data[0,:]))
+            color = iter(plt.cm.rainbow(np.linspace(0,1,len(data[:,0]))))
+            for i in range(len(data[:,0])):
+                c = next(color)
+                mx_idx = np.argmax(data[i])
+                y = data[i,:]
+                plt.plot(x, y, color=c, alpha=0.2, zorder=1)
+                # print(mx_idx, print())
+                # if k=="_val_acc_fold":
+                #     plt.scatter(mx_idx, data[i, mx_idx], color=c, marker="o", s=6, zorder=2)
+                #     plt.scatter(mx_idx, float(test_accs[i]), color=c, marker="x", zorder=3)
+                plt.title(dataset + m + k + str(fold))
         plt.show()
 
 def summarize_res(folder):
@@ -134,11 +118,35 @@ def summerize_HypParams(folder):
     outfile = "out/" + dataset + "/HyperParams.csv"
     with open(outfile, "w") as hyp:
         writer = csv.writer(hyp)
-        writer.writerow(["dataset","model","fold","num_layers","hidden","lr","lr_decay","step_size","num_epochs"])
+        writer.writerow(["dataset","model","fold","num_layers","hidden","lr","lr_decay","step_size","num_epochs","weight_decay"])
         for m in models:
             for fold in range(4):
-                folder, m, fold, opt_hidden, opt_lr, opt_lr_decay, opt_num_epochs, opt_num_layers, opt_step_size = get_opt_param(m, folder, fold)
-                writer.writerow([folder, m, fold, opt_num_layers, opt_hidden, opt_lr, opt_lr_decay, opt_step_size, opt_num_epochs])
+                folder, m, fold, opt_hidden, opt_lr, opt_lr_decay, opt_num_epochs, opt_num_layers, opt_step_size, opt_weight_decay= get_opt_param(m, folder, fold)
+                writer.writerow([folder, m, fold, opt_num_layers, opt_hidden, opt_lr, opt_lr_decay, opt_step_size, opt_num_epochs, opt_weight_decay])
+
+def plot_mean_sd(m, folder):
+    if "paper" in folder:
+        dataset = "paper/"
+        # path = "Hyperparameters/paper/"
+        # folder = "pT1_dataset/graphs/paper-graphs/distance-based_10_13_14_35/"
+    elif "base" in folder:
+        dataset = "base/"
+        # path = "Hyperparameters/base/"
+        # folder = "pT1_dataset/graphs/base-dataset/"
+
+    test_res_file = "out/" + dataset + "/Results.csv"
+    with open(test_res_file, "r") as res_file:
+        reader = csv.reader(res_file)
+        for row in reader:
+            if row[0]==m:
+                split = ["fold 0", "fold 1", "fold 2", "fold 3", "total"]
+                avg_acc = np.array([row[1], row[3], row[5], row[7], row[9]]).astype(np.float)
+                sd = np.array([row[2], row[4], row[6], row[8], row[10]]).astype(np.float)
+                plt.errorbar(split, avg_acc, sd, linestyle="None",marker="o", capsize=3)
+                plt.ylim(0.5, 1)
+                plt.show()
+
+
 
 
 
@@ -167,8 +175,8 @@ if __name__ == "__main__":
     ##############################
     # args = parser.parse_args()
     # summarize_res(args.folder)
-
-    # # python plotResults.py -d paper
+    #
+    # # # python plotResults.py -d paper
 
     #create csv with all HypParams
     # ##############################
@@ -176,3 +184,12 @@ if __name__ == "__main__":
     # summerize_HypParams(args.folder)
 
     # # python plotResults.py -d paper
+
+
+    #plot mean and sds
+    ###############################
+    # parser.add_argument("--model", "-m", type=str, required=True)
+    # args = parser.parse_args()
+    # plot_mean_sd(args.model, args.folder)
+
+    # python plotResults.py -m GCN -d paper
